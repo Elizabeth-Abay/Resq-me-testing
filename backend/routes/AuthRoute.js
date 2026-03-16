960911ec01e28fda321da37a928f7210e8a6e085
Elizabeth <ElizabethAbayZewdie@outlook.com>
2026-03-16 09:20:35 +0300

    Updated resend-email-url-path name

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -13,7 +13,7 @@ let authController = new AuthController();
 
 authRouter.post('/sign-up', signUpInputValidator, authController.signUp); // checked
 authRouter.get('/verify-email', emailInputValidator, authController.validateEmail); // checked
-authRouter.get('/resend-email-verification-link', resendEmailValidator, authController.resendEmailVerificationLink); // test it
+authRouter.get('/resend-verification', resendEmailValidator, authController.resendEmailVerificationLink); // test it
 authRouter.post('/log-in' , logInValidator , authController.logIn); // checked
 authRouter.post('/log-out' , refreshValidator  , logOutValidator , authController.logOut); // checked
 
